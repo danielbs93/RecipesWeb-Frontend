59a247140a0a2690c1f5126be2ca629f3494f42f
Daniel Ben Simon <49146507+danielbs93@users.noreply.github.com>
2020-07-27 21:12:27 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,43 @@
+# Recipes web by Daniel & Eran
+
+ID's: 204554166, 203818729
+
+# Frontend Client
+Developed using Vue.js & Nodejs
+
+## Links
+
+Git [Backend](https://github.com/SISE-Web-Development-Environments/assignment-3-2-V2-daniel-eran)
+
+Swagger [API](https://app.swaggerhub.com/apis-docs/erantoutian/EatAndRepeat/1.0.0#/Recipe)
+
+Server loaded on: [Herokuapp](https://recipe-server-3-3-daniel-eran.herokuapp.com/) but it is down at the moment
+
+
+
+
+## Available Scripts
+
+In this project directory, you can run:
+
+### `npm run serve`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:8080](http://localhost:8080) to view it in the browser.
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+### `npm i`
+
+Builds the app for production to the `build` folder.<br />
+
+The build is minified and the filenames include the hashes.<br />
+Your app is ready to be deployed!
+
+
+## Bonus
+ - Cooking a recipe bonus has been implemented
+ - Remove from favourites has been implemented
+ - Adding a recipe bonus has been implemented
+ 
---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <v-app>
+      <NavBar></NavBar>
+      <Jumbotron></Jumbotron>
+      <router-view />
+      <center><Footer></Footer></center>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/shared/NavBar'
+import Footer from './components/shared/Footer'
+import Jumbotron from './components/shared/Jumbotron'
+export default {
+  name: "App",
+  methods: {
+    Logout() {
+      this.$root.store.logout();
+      this.$root.toast("Logout", "User logged out successfully", "success");
+
+      this.$router.push("/").catch(() => {
+        this.$forceUpdate();
+      });
+    }
+  },
+  components: {
+    NavBar,
+    Footer,
+    Jumbotron
+  },
+};
+
+</script>
+
+<style lang="scss">
+@import "@/scss/form-style.scss";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  min-height: 100vh;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
